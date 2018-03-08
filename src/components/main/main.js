ba23b48c88882a87c00211e56d3a149c04d7da0b
Ben Malin <benjaminamalin@gmail.com>
2018-03-08 12:47:14 -0500

    added product reviews and site footer

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -35,19 +35,28 @@ class Main extends Component {
       {
         id: 1,
         authorName: "Shaun White",
-        authorPicture: "",
+        authorPicture: "./assets/reviewer1.png",
         title: "This Thing is a monster!",
         date: "08/20/2013",
-        stars: 4,
+        stars: [
+          <img index={1} src="./assets/star.png" />,
+          <img index={2} src="./assets/star.png" />,
+          <img index={3} src="./assets/star.png" />,
+          <img index={4} src="./assets/star.png" />
+        ],
         review: `I picked up this jacket for use in San Francisco and the surrounded Bay Area (Lake Tahoe included). I use this jacket for everything. It is often cloudy/drizzly/cold in SF and this jacket deals with it all. I have not tried in heavy rain, but in drizzly conditions, the DWR treatment defininitely makes small drops bead right off. It also has a nice fleece-like liner inside and keeps me really warm down to 40 degree or so. In Tahoe, I have used this with a shirt or base layer, mid layer fleece, and then this jacket. I don't get cold too easily but in temps 20's - 30's I felt great (with some gloves and a beanie of course). I have not tried it skiing yet because I snowboard and to be honest it wouldn't be quite large enough (I like baggier snowboard clothing). But I have seen people who wear softshells skiing all the time. I also use this when I go to work and want a jacket that dresses up nice. With slacks, nice shoes, and a button up it looks great. I am 6' and about 190 lbs (34 in arms) and the large fits perfect. The pockets are well designed as well. The soft shell is the new "do-everything" jacket; wind/water resistent but also maleable and comfy for wearing anywhere. I highly recommend this jacket.`
       },
       {
         id: 2,
         authorName: "Jennie Loren",
-        authorPicture: "",
+        authorPicture: "./assets/reviewer2.png",
         title: "Luv it so much I bought 2",
         date: "01/12/2013",
-        stars: 3,
+        stars: [
+          <img index={1} src="./assets/star.png" />,
+          <img index={2} src="./assets/star.png" />,
+          <img index={3} src="./assets/star.png" />
+        ],
         review: `Loved this jacket so much from previous years I decided to buy a second in a different color. I'm tall and fairly slender and its tailored silhouette fits me nicely. For perspective I'm around 6'3" ~150 lbs. and a large does the trick. I'll wear this for all of fall and into the winter. Despite not being bulky or baggy I find it good and warm for anything down to 40 degrees and perhaps lower if you're keeping up on your layering. For weather at freezing or below you should consider something heavier.`
       }
     ];
@@ -105,6 +114,37 @@ class Main extends Component {
         <main role="main">
           <ProductPage item={item} reviews={itemReviews} />
         </main>
+
+        <footer role="contentinfo" id="site-footer">
+          <div className="content-container" id="footer-content">
+            <img
+              className="logo"
+              src="./assets/axium-logo-small.png"
+              alt="Axium logo"
+            />
+            <div className="footer-menu">
+              <div className="footer-links">
+                <a href="www.facebook.com">
+                  <img
+                    className="facebook-icon"
+                    src="./assets/fb-icon.png"
+                    alt="Facebook link"
+                  />
+                </a>
+                <a hred="www.twitter.com">
+                  <img
+                    className="twitter-icon"
+                    src="./assets/twitter-icon.png"
+                    alt="Twitter link"
+                  />
+                </a>
+                <a>My Account</a> |
+                <a>Cart</a> |
+                <a>Logout</a>
+              </div>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
--- src/components/product-page/product-page.js
+++ src/components/product-page/product-page.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
+import Review from "../review/review";
 import "./product-page.css";
 
 class ProductPage extends Component {
   render() {
     const item = this.props.item;
-    const reviews = this.props.reviews;
-    const itemSpecs = item.specs.map(spec => <li>{spec.toUpperCase()}</li>);
-    const bio = item.itemBio.map(paragraph => <p>{paragraph}</p>);
+    const reviews = this.props.reviews.map((review, index) => (
+      <Review key={index} review={review} />
+    ));
+    const itemSpecs = item.specs.map((spec, index) => (
+      <li key={index}>{spec.toUpperCase()}</li>
+    ));
+    const bio = item.itemBio.map((paragraph, index) => (
+      <p key={index}>{paragraph}</p>
+    ));
+    const sizes = item.sizes.map((size, index) => (
+      <option key={index} value={size}>
+        {size}
+      </option>
+    ));
+    const colors = item.colors.map((color, index) => (
+      <option key={index} value={color}>
+        {color}
+      </option>
+    ));
 
     return (
       <div id="product-page">
@@ -22,7 +39,38 @@ class ProductPage extends Component {
               </div>
               <div className="item-description">
                 <h2>{item.name.toUpperCase()}</h2>
-                <ul class="item-specs">{itemSpecs}</ul>
+                <ul className="item-specs">{itemSpecs}</ul>
+                <div className="item-buttons">
+                  <form role="form">
+                    <div className="buttons-grid styled-select">
+                      <select>
+                        <option value="">Select Size</option>
+                        {sizes}
+                      </select>
+                    </div>
+                    <div className="buttons-grid styled-select">
+                      <select>
+                        <option value="">Select Color</option>
+                        {colors}
+                      </select>
+                    </div>
+                    <div className="buttons-grid">
+                      <input
+                        type="number"
+                        name="quantity"
+                        min="1"
+                        max="20"
+                        placeholder="1"
+                        step="1"
+                      />
+                    </div>
+                    <div className="buttons-grid">
+                      <button type="submit" value="submit">
+                        Add to Cart
+                      </button>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
@@ -37,6 +85,15 @@ class ProductPage extends Component {
               />
             </div>
             <div className="bio-text-container">{bio}</div>
+          </div>
+        </div>
+        <div id="reviews">
+          <div className="content-container">
+            <div className="review-header">
+              <h3>Reviews</h3>
+              <a>Write a review on this product</a>
+            </div>
+            <div className="user-reviews">{reviews}</div>
           </div>
         </div>
       </div>
